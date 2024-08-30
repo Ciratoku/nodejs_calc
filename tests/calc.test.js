20efb9a938f87b4cb4b22cdc1519833f0cf201c7
Ciratoku <edissonman255@gmail.com>
2024-08-30 12:49:39 +0300

    tests

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -1,16 +1,17 @@
 const calculate = require("../calc");
+const operators = require("../operators.js");
 
 describe("evaluate function", () => {
   it("23+5=28", () => {
-    expect(calculate("23+5")).toBe(23 + 5);
+    expect(calculate("23+5", operators)).toBe(23 + 5);
   });
   it("(13+4)*2=34", () => {
-    expect(calculate("(13+4)*2")).toBe((13 + 4) * 2);
+    expect(calculate("(13+4)*2", operators)).toBe((13 + 4) * 2);
   });
   it("(1+3)) -> Error", () => {
-    expect(() => calculate("(1+3))")).toThrow();
+    expect(() => calculate("(1+3))", operators)).toThrow();
   });
   it("(1+3 -> Error", () => {
-    expect(() => calculate("(1+3")).toThrow();
+    expect(() => calculate("(1+3", operators)).toThrow();
   });
 });
